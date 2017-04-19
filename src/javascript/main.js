4900c1c27c9da8c2ea0e13c246b4ad747293650b
LinchSmyth <andrei95-1995@mail.ru>
2017-04-20 00:27:00 +0300

    scroll to content with smooth

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+$(document).ready( function () {  
   $('.replies').slick({
     dots: true,
     autoplay: true,
@@ -6,7 +6,43 @@ $(document).ready( function () {
     initialSlide: rand(1, 4)
   });
   
+  $('.navbar').affix({
+    offset: {
+      top: topContentHeight()
+    } 
+  });
+  
   function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+  
+  function topContentHeight () {
+    return $('.first-content-wrapper').height();
+  }
+  
+  $(window).resize(function(){
+    $('.navbar').affix({
+      offset: {
+        top: topContentHeight()
+      } 
+    });
+  });
+  
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+      // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 })
