e7ca07815cf106e505805fbd8c5ee9904408f5cd
LinchSmyth <andrei95-1995@mail.ru>
2017-04-21 00:09:29 +0300

    improved slideIn effects | js code refactoring

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -45,4 +45,39 @@ $(document).ready( function () {
       });
     } // End if
   });
+  
+  // appearing effect for otzivi
+  
+  function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  const sliderImages = document.querySelectorAll('.slide-in');
+  function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+      // half way through the image
+      var sliderImageHeight = $(sliderImage).height();
+      const slideInAt = (window.scrollY + window.innerHeight) - (sliderImageHeight / 2);
+      //bottom of the image
+      const imageBottom = sliderImage.offsetTop + sliderImageHeight;
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY > imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active');
+      } else {
+        sliderImage.classList.remove('active');
+      }
+    });
+  }
+  window.addEventListener('scroll', debounce(checkSlide, 15));
 })
